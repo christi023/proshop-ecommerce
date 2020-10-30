ac5368e07cff94cef8f863cbbb28db593aea663b
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-10-30 14:34:22 +0100

    Added Ratings for each products

--- frontend/src/components/Rating.jsx
+++ frontend/src/components/Rating.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
 const Rating = ({ value, text, color }) => {
   return (
@@ -53,10 +52,4 @@ Rating.defaultProps = {
   color: '#f8e825',
 };
 
-// prop types
-Rating.propTypes = {
-  value: PropTypes.number.isRequired,
-  text: PropTypes.string.isRequired,
-  color: PropTypes.string,
-};
 export default Rating;
