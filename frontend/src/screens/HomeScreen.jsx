4410c783ff539204489e06dacae5a2b15d99df50
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-11-04 17:23:57 +0100

    Loader and Message for Spinner added

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -3,6 +3,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 // components
 import Product from '../components/Product';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+// actions
 import { listProducts } from '../actions/productActions';
 
 const HomeScreen = () => {
@@ -19,9 +22,9 @@ const HomeScreen = () => {
     <>
       <h1>Latest Products</h1>
       {loading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant="danger">{error}</Message>
       ) : (
         <Row>
           {products.map((product) => (
