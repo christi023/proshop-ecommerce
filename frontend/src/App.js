6a4bc655df2e6c681758bc7271e27e9816a8972c
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-11-06 16:40:55 +0100

    Add CartScreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 // components
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+// screens
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Pro Shop </h1>
+          <Route exact path="/" component={HomeScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
