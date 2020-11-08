1286b75089886aad03c7e16f7f069eaae2a1d166
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-11-08 19:54:28 +0100

    provider & store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// connects with redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+// style
 import './bootstrap.min.css';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
