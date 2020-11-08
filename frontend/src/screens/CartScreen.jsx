8031abf36d1c35365d4bc34ca3aba6d5d759acdf
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-11-08 18:56:21 +0100

    Add To Cart Fuctionality

--- frontend/src/screens/CartScreen.jsx
+++ frontend/src/screens/CartScreen.jsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap';
+import Message from '../components/Message';
+// actions
+import { addToCart } from '../actions/cartActions';
 
-const CartScreen = () => {
+const CartScreen = ({ match, location, history }) => {
+  // get id
+  const productId = match.params.id;
+  // get quantity
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1;
+  // define dispatch
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  useEffect(() => {
+    if (productId) {
+      dispatch(addToCart(productId, qty));
+    }
+  }, [dispatch, productId, qty]);
+
   return <div> Cart</div>;
 };
 
