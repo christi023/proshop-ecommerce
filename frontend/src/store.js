eae3390bd5e851fd2cf686e2de64e88868846cde
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-11-08 18:56:59 +0100

    Add To Cart Fuctionality

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,13 +4,20 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import { productListReducer, productDetailsReducer } from './reducers/productReducers';
+import { cartReducer } from './reducers/cartReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 
-const initialState = {};
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : [];
+// get cart items
+
+const initialState = { cart: { cartItems: cartItemsFromStorage } };
 
 const middleware = [thunk];
 
