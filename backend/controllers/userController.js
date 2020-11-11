7314339d119bf50b62128e9f91ddc4ca75849207
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-11-11 14:20:12 +0100

    User controller route function

---
+++ backend/controllers/userController.js
@@ -0,0 +1,8 @@
+import asyncHandler from 'express-async-handler';
+// models
+import User from '../models/userModel.js';
+
+//@desc Auth user & get token
+//@route POST /api/users/login
+//@access Public
+const authUser = asyncHandler(async (req, res) => {});
